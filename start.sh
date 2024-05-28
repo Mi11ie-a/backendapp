b3185fc94b867d2a8318914968555b5d7e0c7478
Mi11ie-a <66948271+Mi11ie-a@users.noreply.github.com>
2024-05-28 18:48:01 +1000

    fixed start script file

--- start.sh
+++ start.sh
@@ -8,19 +8,16 @@ appport=$4
 jwtexp=$5
 
 
-
 sed -i -e "s/\(DB_HOST=\).*/\1$1/" \
 -e "s/\(DB_PORT=\).*/\1$2/" \
 -e "s/\(DB_USERNAME=\).*/\1$3/" \
 -e "s/\(JWT_EXPIRY=\).*/\1$5/" \
 -e "s/\(APP_PORT=\).*/\1$4/" \
 -e "s/\(SECRET_KEY=\).*/\/" \
--e "s/\(DB_PASSWORD=\).*/\/" \
-
- xyz.cfg
+-e "s/\(DB_PASSWORD=\).*/\/" \ xyz.cfg
 
 echo "Starting express application"
-echo "Port: $1"
-echo "DB: $2:$3"
+echo "Port: $4"
+echo "DB: $1:$2@$3"
 
 node src/API/Index.ts

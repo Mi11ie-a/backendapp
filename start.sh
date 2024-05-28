2c39eabf4e8749fd59f0227a09c8db8d2128a5bb
Mi11ie-a <66948271+Mi11ie-a@users.noreply.github.com>
2024-05-28 18:53:22 +1000

    actually fixed

--- start.sh
+++ start.sh
@@ -12,9 +12,7 @@ sed -i -e "s/\(DB_HOST=\).*/\1$1/" \
 -e "s/\(DB_PORT=\).*/\1$2/" \
 -e "s/\(DB_USERNAME=\).*/\1$3/" \
 -e "s/\(JWT_EXPIRY=\).*/\1$5/" \
--e "s/\(APP_PORT=\).*/\1$4/" \
--e "s/\(SECRET_KEY=\).*/\/" \
--e "s/\(DB_PASSWORD=\).*/\/" xyz.cfg
+-e "s/\(APP_PORT=\).*/\1$4/" .env
 
 echo "Starting express application"
 echo "Port: $4"
